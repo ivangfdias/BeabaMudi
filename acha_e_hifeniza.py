bb2f27fe30e353be6e57466bcd39db4c52555458
Ivan Dias / PipsQueen <ivangfdias@gmail.com>
2019-10-21 23:01:32 -0300

    Atualização: Main, nuBank, acha_e_hifeniza
    Main.cpp agora escreve letras acentuadas e c-cedilhas em TextoGerado. Não há certeza de portabilidade.
    acha_e_hifeniza.py usa agora o léxico "machado", da obra completa de Machado de Assis, e usa mais palavras
    nuBank.txt atualizado para condizer com as mudanças de acha_e_hifeniza.py

--- acha_e_hifeniza.py
+++ acha_e_hifeniza.py
@@ -4,16 +4,18 @@ from nltk.probability import *
 import nltk.corpus
 import re
 import string
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
 
 dic = pyphen.Pyphen(lang='pt-BR')
 escrita=[]
 banco = open("nuBank.txt", 'w', encoding='utf-8')
-corpus = nltk.corpus.floresta
+corpus = nltk.corpus.machado
 corpus.ensure_loaded()
 freq = nltk.FreqDist(corpus.words()) 
-for palavra, frequencia in freq.most_common(30000):
+for palavra, frequencia in freq.most_common(42000):
     palavra = re.sub('['+string.punctuation+']', '', palavra)
-    if not palavra.isnumeric() and palavra is not "" and "truiu" not in palavra and palavra is not "art":
+    if palavra is not "" and "truiu" not in palavra and palavra is not "art" and not hasNumbers(palavra) and palavra not in escrita:
         escrita.append(palavra)
         escrita.append("\n")
 for palavra in escrita:
