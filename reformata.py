b07b1087c03ef2f25ae4c33d72b8cb29b6314241
Ivan Dias / PipsQueen <ivangfdias@gmail.com>
2019-12-14 18:05:12 -0300

    Update reformata.py

--- reformata.py
+++ reformata.py
@@ -4,7 +4,9 @@ import titulo
 num_sobrenomes = random.randint(1,3)
 nomeCompleto = nome.nome()
 nomeCompleto = nomeCompleto + " " + nome.sobrenome(num_sobrenomes)
-print(nomeCompleto)
+arquivo = open("AutorGerado.txt", "w", encoding = "utf-8")
+arquivo.write(nomeCompleto)
+arquivo.close
 poema = []
 arquivo = open("TextoGerado.txt", 'r', encoding = "utf-8")
 for linha in arquivo:
@@ -14,6 +16,5 @@ tituloFeito = titulo.titulo()
 arquivo = open("PoemaFormatado.txt", 'w', encoding = "utf-8")
 for verso in poema:
     arquivo.write(verso)
-arquivo.write("\n\n"+nomeCompleto)
 arquivo.close
 titulo.titulo()
