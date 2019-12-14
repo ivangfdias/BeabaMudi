d35623fb4e7898023aa59d31a57db4251502396d
Ivan Dias / PipsQueen <ivangfdias@gmail.com>
2019-12-14 17:58:25 -0300

    Update nome.py

--- nome.py
+++ nome.py
@@ -1,6 +1,6 @@
 import subprocess
 import time
-process = subprocess.Popen([r"cmd"])
+#process = subprocess.Popen([r"cmd"])
 
 def strip_end(text, suffix):
     if not text.endswith(suffix):
@@ -8,23 +8,23 @@ def strip_end(text, suffix):
     return text[:len(text)-len(suffix)]
 
 def nome():
-    sub = subprocess.Popen([r"./geraNome"])
+    sub = subprocess.Popen(["./geraNome"])
     arquivo = open("nomeGerado.txt", "r", encoding = "utf-8")
     for line in arquivo:
         novoNome = line
         novoNome = strip_end(novoNome, "\n")
     arquivo.close
-    process.terminate()
+    #process.terminate()
     return novoNome.title()
 
 def sobrenome(quantidade):
-    sub = subprocess.Popen([r"./geraSobrenome"])
+    sub = subprocess.Popen(["./geraSobrenome"])
     sub.communicate(str(quantidade))
     arquivo = open("sobrenomeGerado.txt", "r", encoding = "utf-8")
     novoNome = ""
     for line in arquivo:
         novoNome = novoNome + strip_end(line, "\n") + " "
     arquivo.close   
-    process.terminate()
+    #process.terminate()
     novoNome = strip_end(novoNome, " ")
     return novoNome.title()
